795341cda74062acc94d24b763aa0bae839f63e2
Howe <1013410795@qq.com>
2018-12-25 22:34:44 +0800

    add class triplet selector

---
+++ utils.py
@@ -0,0 +1,86 @@
+from itertools import combinations
+import numpy as np
+import torch
+
+def pdist(vectors):
+    distance_matrix = -2 * vectors.mm(torch.t(vectors)) + vectors.pow(2).sum(dim=1).view(1, -1) + vectors.pow(2).sum(
+        dim=1).view(-1, 1)
+    return distance_matrix
+
+
+def hardest_negative(loss_values):
+    hard_negative = np.argmax(loss_values)
+    return hard_negative if loss_values[hard_negative] > 0 else None
+
+
+def random_hard_negative(loss_values):
+    hard_negatives = np.where(loss_values > 0)[0]
+    return np.random.choice(hard_negatives) if len(hard_negatives) > 0 else None
+
+
+def semihard_negative(loss_values, margin):
+    semihard_negatives = np.where(np.logical_and(loss_values < margin, loss_values > 0))[0]
+    return np.random.choice(semihard_negatives) if len(semihard_negatives) > 0 else None
+
+
+class FunctionNegativeTripletSelector(object):
+    def __init__(self, margin, negative_selection_fn, cpu=True):
+        self.cpu = cpu
+        self.margin = margin
+        self.negative_selection_fn = negative_selection_fn
+
+    def get_triplets(self, embeddings, labels):
+        if self.cpu:
+            embeddings = embeddings.cpu()
+        distance_matrix = pdist(embeddings)
+        distance_matrix = distance_matrix.cpu()
+
+        labels = labels.cpu().data.numpy()
+        triplets = []
+
+        for label in set(labels):
+            label_mask = (labels == label)
+            label_indices = np.where(label_mask)[0]
+            if len(label_indices) < 2:
+                continue
+            negative_indices = np.where(np.logical_not(label_mask))[0]
+            anchor_positives = list(combinations(label_indices, 2))  # All anchor-positive pairs
+            anchor_positives = np.array(anchor_positives)
+
+            ap_distances = distance_matrix[anchor_positives[:, 0], anchor_positives[:, 1]]
+            for anchor_positive, ap_distance in zip(anchor_positives, ap_distances):
+                loss_values = ap_distance - distance_matrix[
+                    torch.LongTensor(np.array([anchor_positive[0]])), torch.LongTensor(negative_indices)] + self.margin
+                loss_values = loss_values.data.cpu().numpy()
+                hard_negative = self.negative_selection_fn(loss_values)
+                if hard_negative is not None:
+                    hard_negative = negative_indices[hard_negative]
+                    triplets.append([anchor_positive[0], anchor_positive[1], hard_negative])
+
+        if len(triplets) == 0:
+            triplets.append([anchor_positive[0], anchor_positive[1], negative_indices[0]])
+
+        triplets = np.array(triplets)
+
+        return torch.LongTensor(triplets)
+
+
+def HardestNegativeTripletSelector(margin, cpu=False):
+    return FunctionNegativeTripletSelector(margin=margin, negative_selection_fn=hardest_negative, cpu=cpu)
+
+
+def RandomNegativeTripletSelector(margin, cpu=False):
+    return FunctionNegativeTripletSelector(margin=margin, negative_selection_fn=random_hard_negative, cpu=cpu)
+
+def SemihardNegativeTripletSelector(margin, cpu=False):
+    return FunctionNegativeTripletSelector(margin=margin, negative_selection_fn=lambda x: semihard_negative(x, margin), cpu=cpu)
+
+
+if __name__ == '__main__':
+    from data_manager.datasets_wrapper import BalanceBatchSampler
+    from data_manager.datasets import DevSet
+    from torch.utils.data import DataLoader
+
+    train_dataset = DevSet(mode='train', device='a')
+    train_batch_sampler = BalanceBatchSampler(dataset=train_dataset, n_classes=10, n_samples=6)
+    train_loader = DataLoader(dataset=train_dataset, batch_sampler=train_batch_sampler, num_workers=1)
