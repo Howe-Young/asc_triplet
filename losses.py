7eb01abaa19767d9271a05e209b3c9b7d0f42e96
Howe <1013410795@qq.com>
2018-12-25 22:33:33 +0800

    create loss.py which include triplet loss

---
+++ losses.py
@@ -0,0 +1,26 @@
+"""
+some loss class
+
+"""
+
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+class OnlineTripletLoss(nn.Module):
+    def __init__(self, margin, triplet_selector):
+        super(OnlineTripletLoss, self).__init__()
+        self.margin = margin
+        self.triplet_selector = triplet_selector
+
+    def forward(self, embeddings, target):
+        triplets = self.triplet_selector.get_triplets(embeddings, target)
+
+        if embeddings.is_cuda:
+            triplets = triplets.cuda()
+
+        ap_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 1]]).pow(2).sum(1)
+        an_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 2]]).pow(2).sum(1)
+        losses = F.relu(ap_distances - an_distances + self.margin)
+
+        return losses.mean(), len(triplets)
