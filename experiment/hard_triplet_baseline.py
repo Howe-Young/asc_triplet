77d2c8232dd091a7660604fff943c7c321095b20
Howe <1013410795@qq.com>
2018-12-26 23:04:36 +0800

    add log_interval parameter.

--- experiment/hard_triplet_baseline.py
+++ experiment/hard_triplet_baseline.py
@@ -15,7 +15,7 @@ from torch.optim import lr_scheduler
 from trainer import fit
 
 
-def hard_triplet_baseline_exp(device='3', lr=1e-3, n_epochs=300, n_classes=10, n_samples=12, margin=0.3):
+def hard_triplet_baseline_exp(device='3', lr=1e-3, n_epochs=300, n_classes=10, n_samples=12, margin=0.3, log_interval=50):
     """
 
     :param device:
@@ -48,14 +48,12 @@ def hard_triplet_baseline_exp(device='3', lr=1e-3, n_epochs=300, n_classes=10, n
     test_batch_sampler = BalanceBatchSampler(dataset=test_dataset, n_classes=n_classes, n_samples=n_samples)
     test_batch_loader = DataLoader(dataset=test_dataset, batch_sampler=test_batch_sampler, num_workers=1)
 
-
     model = vggish_bn()
-    model.cuda()
+    model = model.cuda()
     loss_fn = OnlineTripletLoss(margin=margin, triplet_selector=RandomNegativeTripletSelector(margin=margin))
     optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = lr_scheduler.StepLR(optimizer=optimizer, step_size=30, gamma=0.5)
 
-    log_interval = 20
     fit(train_loader=train_batch_loader, val_loader=test_batch_loader, model=model, loss_fn=loss_fn,
         optimizer=optimizer, scheduler=scheduler, n_epochs=n_epochs, log_interval=log_interval,
         metrics=[AverageNoneZeroTripletsMetric()])
@@ -68,18 +66,8 @@ if __name__ == '__main__':
         'n_epochs': 300,
         'n_classes': 10,
         'n_samples': 12,
-        'margin': 0.3
+        'margin': 0.3,
+        'log_interval': 50
     }
 
     hard_triplet_baseline_exp(**kwargs)
-
-    # os.environ['CUDA_VISIBLE_DEVICES'] = str(2)
-
-    # train_loader = DataLoader(dataset=train_dataset, batch_size=128, shuffle='False', num_workers=1)
-
-
-
-    # for batch_id, (data, label) in enumerate(train_batch_loader):
-    #     print("batch id: ", batch_id)
-    #     print("data: ", data.shape)
-    #     print('label', label)
