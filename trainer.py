745e3ae5c339106823a3a2dc6349352efaa29896
Howe <1013410795@qq.com>
2018-12-28 22:30:23 +0800

    remove total triplets number

--- trainer.py
+++ trainer.py
@@ -28,7 +28,7 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
 
         message = 'Epoch: {}/{}. Train set: Average loss: {:.4f}.'.format(epoch + 1, n_epochs, train_loss)
         for metric in metrics:
-            message += '\t{}: {}/{}'.format(metric.name(), metric.value(), metric.total())
+            message += '\t{}: {}'.format(metric.name(), metric.value())
 
         # test stage
         val_loss, metrics = test_epoch(val_loader, model, loss_fn, metrics)
@@ -36,7 +36,7 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
 
         message += '\nEpoch: {}/{}. Validation set: Average loss: {:.4f}'.format(epoch + 1, n_epochs, val_loss)
         for metric in metrics:
-            message += '\t{}: {}/{}'.format(metric.name(), metric.value(), metric.total())
+            message += '\t{}: {}'.format(metric.name(), metric.value())
 
         print(message)
 
@@ -82,7 +82,7 @@ def train_epoch(train_loader, model, loss_fn, optimizer, log_interval, metrics):
             message = 'Train: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(batch_idx * len(data[0]), len(train_loader.dataset),
                                                                       100 * batch_idx / len(train_loader), np.mean(losses))
             for metric in metrics:
-                message += '\t{}: {}/{}'.format(metric.name(), metric.value(), metric.total())
+                message += '\t{}: {}'.format(metric.name(), metric.value())
 
             print(message)
             losses = []
