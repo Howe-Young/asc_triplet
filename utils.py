92104b74939fd6a74f732455d76444cc6c672a3f
Howe <1013410795@qq.com>
2018-12-28 16:50:48 +0800

    add visualization module

--- utils.py
+++ utils.py
@@ -3,12 +3,26 @@ import numpy as np
 import torch
 
 def pdist(vectors):
+    """
+    vectors shape: (batch_size, k-dimensions)
+    compute the Euclidean distance between any two pairs.
+    using formula distance = ||A - B||^2 = A'A + B'B - 2A'B
+    note: A, B represent vector, respectively. A' denotes transpose of A
+    :param vectors: embeddings of batch_size samples.
+    :return: distance matrix
+    """
     distance_matrix = -2 * vectors.mm(torch.t(vectors)) + vectors.pow(2).sum(dim=1).view(1, -1) + vectors.pow(2).sum(
         dim=1).view(-1, 1)
     return distance_matrix
 
 
 def hardest_negative(loss_values):
+    """
+    According distance choose min distance between anchor and negative sample.
+    When takes maximum loss_values, the sample is hardest.
+    :param loss_values:
+    :return:
+    """
     hard_negative = np.argmax(loss_values)
     return hard_negative if loss_values[hard_negative] > 0 else None
 
@@ -24,6 +38,12 @@ def semihard_negative(loss_values, margin):
 
 
 class FunctionNegativeTripletSelector(object):
+    """
+    for each positive pair, takes the hardest negative sample (with the greatest triplet loss value) to create a triplet
+    Margin should match the margin used in triplet loss.
+    negative_selection_fn should take array of loss_values for a given anchor-positive pair and all negative samples
+    and return a negative index for that pair.
+    """
     def __init__(self, margin, negative_selection_fn, cpu=True):
         self.cpu = cpu
         self.margin = margin
@@ -37,6 +57,7 @@ class FunctionNegativeTripletSelector(object):
 
         labels = labels.cpu().data.numpy()
         triplets = []
+        triplets_count = 0
 
         for label in set(labels):
             label_mask = (labels == label)
@@ -46,6 +67,7 @@ class FunctionNegativeTripletSelector(object):
             negative_indices = np.where(np.logical_not(label_mask))[0]
             anchor_positives = list(combinations(label_indices, 2))  # All anchor-positive pairs
             anchor_positives = np.array(anchor_positives)
+            triplets_count += anchor_positives.shape[0]
 
             ap_distances = distance_matrix[anchor_positives[:, 0], anchor_positives[:, 1]]
             for anchor_positive, ap_distance in zip(anchor_positives, ap_distances):
@@ -62,7 +84,7 @@ class FunctionNegativeTripletSelector(object):
 
         triplets = np.array(triplets)
 
-        return torch.LongTensor(triplets)
+        return torch.LongTensor(triplets), triplets_count
 
 
 def HardestNegativeTripletSelector(margin, cpu=False):
@@ -72,8 +94,46 @@ def HardestNegativeTripletSelector(margin, cpu=False):
 def RandomNegativeTripletSelector(margin, cpu=False):
     return FunctionNegativeTripletSelector(margin=margin, negative_selection_fn=random_hard_negative, cpu=cpu)
 
+
 def SemihardNegativeTripletSelector(margin, cpu=False):
     return FunctionNegativeTripletSelector(margin=margin, negative_selection_fn=lambda x: semihard_negative(x, margin), cpu=cpu)
+
+
+# visualization module
+import matplotlib.pyplot as plt
+asc_classes = ['0', '1', '2', '3', '4', '5', '6', '7','8', '9']
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+              '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+              '#bcbd22', '#17becf']
+
+
+def plot_embeddings(embeddings, targets, xlim=None, ylim=None, title=None):
+    plt.figure(figsize=(10, 10))
+    for i in range(10):
+        inds = np.where(targets == i)[0]
+        plt.scatter(embeddings[inds, 0], embeddings[inds, 1], c=colors[i])
+
+    if xlim:
+        plt.xlim(xlim[0], xlim[1])
+    if ylim:
+        plt.ylim(ylim[0], ylim[1])
+    plt.legend(asc_classes)
+    if title:
+        plt.title(title)
+    plt.show()
+
+def extract_embeddings(dataloader, model):
+    with torch.no_grad():
+        model.eval()
+        embeddings = np.zeros([len(dataloader.dataset), 2])
+        labels = np.zeros(len(dataloader.dataset))
+        k = 0
+        for images, target in dataloader:
+            images = images.cuda()
+            embeddings[k:k+len(images)] = model.get_embeddings(images).data.cpu().numpy()
+            labels[k:k+len(images)] = target.numpy()
+            k += len(images)
+        return embeddings, labels
 
 
 if __name__ == '__main__':
