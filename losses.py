5c46079d1bb08ecf3639fbd995f0e07c9981345a
Howe <1013410795@qq.com>
2018-12-28 17:05:27 +0800

    add comments

--- losses.py
+++ losses.py
@@ -8,13 +8,18 @@ import torch
 import torch.nn.functional as F
 
 class OnlineTripletLoss(nn.Module):
+    """
+    online triplet loss
+    Takes a batch of embeddings and corresponding labels.
+    Triplets are generated using triplet_selector object that take embeddings and targets and return indices of triplets.
+    """
     def __init__(self, margin, triplet_selector):
         super(OnlineTripletLoss, self).__init__()
         self.margin = margin
         self.triplet_selector = triplet_selector
 
     def forward(self, embeddings, target):
-        triplets = self.triplet_selector.get_triplets(embeddings, target)
+        triplets, triplets_count = self.triplet_selector.get_triplets(embeddings, target)
 
         if embeddings.is_cuda:
             triplets = triplets.cuda()
@@ -23,4 +28,4 @@ class OnlineTripletLoss(nn.Module):
         an_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 2]]).pow(2).sum(1)
         losses = F.relu(ap_distances - an_distances + self.margin)
 
-        return losses.mean(), len(triplets)
+        return losses.mean(), len(triplets), triplets_count
