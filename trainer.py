385793ea9c2d7536ac79ad7ab3aac12920fbd88e
Howe <1013410795@qq.com>
2019-01-07 20:05:50 +0800

    add checkpoint part

--- trainer.py
+++ trainer.py
@@ -2,7 +2,8 @@ import torch
 import numpy as np
 
 
-def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs, log_interval, metrics=[], start_epoch=0):
+def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs, log_interval, metrics=[], start_epoch=0,
+        train_hist=None, val_hist=None, ckpter=None, logging=None):
     """
     fit model and test model
     :param train_loader:
@@ -20,25 +21,44 @@ def fit(train_loader, val_loader, model, loss_fn, optimizer, scheduler, n_epochs
     for epoch in range(0, start_epoch):
         scheduler.step()
 
-    for epoch in range(start_epoch, n_epochs):
+    for epoch in range(start_epoch + 1, n_epochs + 1):
         scheduler.step()
 
         # train stage
         train_loss, metrics = train_epoch(train_loader, model, loss_fn, optimizer, log_interval, metrics)
-
-        message = 'Epoch: {}/{}. Train set: Average loss: {:.4f}.'.format(epoch + 1, n_epochs, train_loss)
+        train_logs = dict()
+        train_logs['loss'] = train_loss
         for metric in metrics:
-            message += '\t{}: {}'.format(metric.name(), metric.value())
+            train_logs[metric.name()] = metric.value()
+        if train_hist is not None:
+            train_hist.add(logs=train_logs, epoch=epoch)
+        # message = 'Epoch: {}/{}. Train set: Average loss: {:.4f}.'.format(epoch + 1, n_epochs, train_loss)
+        # for metric in metrics:
+        #     message += '\t{}: {}'.format(metric.name(), metric.value())
 
         # test stage
         val_loss, metrics = test_epoch(val_loader, model, loss_fn, metrics)
         val_loss /= len(val_loader)
-
-        message += '\nEpoch: {}/{}. Validation set: Average loss: {:.4f}'.format(epoch + 1, n_epochs, val_loss)
+        val_logs = dict()
+        val_logs['loss'] = val_loss
         for metric in metrics:
-            message += '\t{}: {}'.format(metric.name(), metric.value())
+            val_logs[metric.name()] = metric.value()
+        if val_hist is not None:
+            val_hist.add(logs=val_logs, epoch=epoch)
 
-        print(message)
+            train_hist.clear()
+            train_hist.plot()
+            val_hist.plot()
+        if logging is not None:
+            logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, train_hist.name, str(train_hist.recent)))
+            logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, val_hist.name, str(val_hist.recent)))
+        if ckpter is not None:
+            ckpter.check_on(epoch=epoch, monitor='acc', loss_acc=val_logs)
+        # message += '\nEpoch: {}/{}. Validation set: Average loss: {:.4f}'.format(epoch + 1, n_epochs, val_loss)
+        # for metric in metrics:
+        #     message += '\t{}: {}'.format(metric.name(), metric.value())
+        #
+        # print(message)
 
 
 def train_epoch(train_loader, model, loss_fn, optimizer, log_interval, metrics):
