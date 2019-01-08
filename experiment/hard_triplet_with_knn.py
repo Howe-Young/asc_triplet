eab45d12d843830a61b7e09c34c58dde4a3e6922
Howe <1013410795@qq.com>
2019-01-08 09:08:42 +0800

    init knn classifier

--- experiment/hard_triplet_with_knn.py
+++ experiment/hard_triplet_with_knn.py
@@ -18,22 +18,24 @@ from verification import *
 import logging
 from utils.history import *
 from utils.checkpoint import *
+from utils.utilities import *
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def hard_triplet_baseline_exp(device='3', ckpt_prefix='Run01', lr=1e-3, n_epochs=300, n_classes=10, n_samples=12,
-                              margin=0.3, log_interval=50, log_level="INFO"):
+def hard_triplet_with_knn_exp(device='3', ckpt_prefix='Run01', lr=1e-3, embedding_epochs=10, n_epochs=100, n_classes=10, n_samples=12,
+                              margin=0.3, log_interval=50, log_level="INFO", k=3):
     """
-
+    knn as classifier.
     :param device:
     :param lr:
     :param n_epochs:
     :param n_classes:
     :param n_samples:
+    :param k: kNN parameter
     :return:
     """
     kwargs = locals()
-    log_file = '{}/ckpt/hard_triplet_baseline_exp/{}.log'.format(ROOT_DIR, ckpt_prefix)
+    log_file = '{}/ckpt/hard_triplet_with_knn_exp/{}.log'.format(ROOT_DIR, ckpt_prefix)
     if not os.path.exists(os.path.dirname(log_file)):
         os.makedirs(os.path.dirname(log_file))
     logging.basicConfig(filename=log_file, level=getattr(logging, log_level.upper(), None))
@@ -68,45 +70,50 @@ def hard_triplet_baseline_exp(device='3', ckpt_prefix='Run01', lr=1e-3, n_epochs
     scheduler = lr_scheduler.StepLR(optimizer=optimizer, step_size=30, gamma=0.5)
 
     fit(train_loader=train_batch_loader, val_loader=test_batch_loader, model=model, loss_fn=loss_fn,
-        optimizer=optimizer, scheduler=scheduler, n_epochs=n_epochs, log_interval=log_interval,
+        optimizer=optimizer, scheduler=scheduler, n_epochs=embedding_epochs, log_interval=log_interval,
         metrics=[AverageNoneZeroTripletsMetric()])
 
-    verification(model=model)
+    # verification(model=model)
     train_embedding_tl, train_labels_tl = extract_embeddings(train_batch_loader, model, 64)
-    # utils.plot_embeddings(embeddings=train_embedding_tl, targets=train_labels_tl, title='train set')
+    # plot_embeddings(embeddings=train_embedding_tl, targets=train_labels_tl, title='train set')
     test_embedding_tl, test_labels_tl = extract_embeddings(test_batch_loader, model, 64)
-    # utils.plot_embeddings(embeddings=test_embedding_tl, targets=test_labels_tl, title='test set')
+    # plot_embeddings(embeddings=test_embedding_tl, targets=test_labels_tl, title='test set')
 
-    model2 = networks.classifier()
-    model2 = model2.cuda()
-    loss_fn2 = nn.CrossEntropyLoss()
-    optimizer2 = optim.Adam(model2.parameters(), lr=lr)
-    scheduler2 = lr_scheduler.StepLR(optimizer=optimizer2, step_size=30, gamma=0.5)
-    train_dataset2 = DatasetWrapper(data=train_embedding_tl, labels=train_labels_tl, transform=ToTensor())
-    test_dataset2 = DatasetWrapper(data=test_embedding_tl, labels=test_labels_tl, transform=ToTensor())
-    train_loader2 = DataLoader(dataset=train_dataset2, batch_size=128, shuffle=True, num_workers=1)
-    test_loader2 = DataLoader(dataset=test_dataset2, batch_size=128, shuffle=False, num_workers=1)
+    distance_matrix = get_distance_matrix2(test_embedding_tl, train_embedding_tl)
+    sorted_index = np.argsort(distance_matrix, axis=1)
+    predict_labels = []
+    for i in range(len(test_embedding_tl)):
+        class_cnt = np.zeros([10])
+        k_neighbor = train_labels_tl[sorted_index[i]]
+        for j in range(k):
+            # print(k_neighbor[j])
+            class_cnt[int(k_neighbor[j])] += 1
+        predict_labels.append(np.max(class_cnt))
+    # print("test_labels:", test_labels_tl, 'len: ', len(test_labels_tl))
+    # print("predict_labels: ", predict_labels)
+    # print('len', len(predict_labels))
+    predict_labels = np.array(predict_labels)
+    # print(type(test_labels_tl))
+    test_acc = (test_labels_tl == predict_labels).sum() / len(test_labels_tl)
+    print("Test Accuracy: ", test_acc)
 
-    train_hist = History(name='train/a')
-    val_hist = History(name='val/a')
-    ckpter = CheckPoint(model=model, optimizer=optimizer, path='{}/ckpt/hard_triplet_baseline_exp'.format(ROOT_DIR),
-                                  prefix=ckpt_prefix, interval=1, save_num=1)
-    fit(train_loader=train_loader2, val_loader=test_loader2, model=model2, loss_fn=loss_fn2,
-        optimizer=optimizer2, scheduler=scheduler2, n_epochs=n_epochs, log_interval=log_interval,
-        metrics=[AccumulatedAccuracyMetric()], train_hist=train_hist, val_hist=val_hist, ckpter=ckpter, logging=logging)
+
 
 
 
 if __name__ == '__main__':
 
     kwargs = {
-        'device': '2',
+        'ckpt_prefix': 'Run01',
+        'device': '0',
         'lr': 1e-3,
-        'n_epochs': 5,
+        'embedding_epochs': 10,
+        'n_epochs': 50,
         'n_classes': 10,
         'n_samples': 12,
         'margin': 1.0,
-        'log_interval': 80
+        'log_interval': 80,
+        'k': 5
     }
 
-    hard_triplet_baseline_exp(**kwargs)
+    hard_triplet_with_knn_exp(**kwargs)
