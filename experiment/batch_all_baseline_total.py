eeb50a0ac8a593689940a724688d41735845098e
Howe <1013410795@qq.com>
2019-01-13 21:12:56 +0800

    add device bc

--- experiment/batch_all_baseline_total.py
+++ experiment/batch_all_baseline_total.py
@@ -26,7 +26,9 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
                                  n_classes=10, n_samples=12, batch_size=128, margin=0.3, log_interval=50,
                                  log_level="INFO", k=3, squared=False, embed_dims=64, embed_net='vgg'):
     """
-    knn as classifier.
+    Using the entire data set, including device A, B, C.
+    Using the batch all method to select the triplets, kNN as the verification
+    Finally, using a linear classifier to classify the extraction into embeddings.
     :param device:
     :param lr:
     :param n_epochs:
@@ -58,8 +60,11 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
         Normalize(mean=mu, std=sigma),
         ToTensor()
     ]))
+
+    # get the normalized test dataset
     test_dataset = {}
-    for device in ['a', 'b', 'c']:
+    device_list = ['a', 'b', 'c', 'bc']
+    for device in device_list:
         test_dataset[device] = DevSet(mode='test', device=device, transform=Compose([
             Normalize(mean=mu, std=sigma),
             ToTensor()
@@ -69,15 +74,10 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
     train_batch_loader = DataLoader(dataset=train_dataset, batch_sampler=train_batch_sampler, num_workers=1)
 
     test_loader = {}
-    for device in ['a', 'b', 'c']:
+    for device in device_list:
         test_loader[device] = DataLoader(dataset=test_dataset[device], batch_size=batch_size, shuffle=False, num_workers=1)
-        # test_batch_sampler[device] = BalanceBatchSampler(dataset=test_dataset[device], n_classes=n_classes,
-        #                                                  n_samples=n_samples)
-        # test_batch_loader[device] = DataLoader(dataset=test_batch_sampler[device],
-        #                                        batch_sampler=test_batch_sampler[device], num_workers=1)
-    # test_batch_sampler = BalanceBatchSampler(dataset=test_dataset, n_classes=n_classes, n_samples=n_samples)
-    # test_batch_loader = DataLoader(dataset=test_dataset, batch_sampler=test_batch_sampler, num_workers=1)
 
+    # network architecture
     if embed_net == 'vgg':
         model = networks.vggish_bn()
     elif embed_net == 'shallow':
@@ -85,6 +85,8 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
     else:
         print("{} network doesn't exist.".format(embed_net))
         return
+
+    # to gpu
     model = model.cuda()
     loss_fn = HardTripletLoss(margin=margin, hardest=False, squared=squared)
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -92,23 +94,25 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
 
     train_hist = History(name='train/a')
     val_hist = {}
-    val_hist['a'], val_hist['b'], val_hist['c'] = History(name='test/a'), History(name='test/b'), History(name='test/c')
+    for device in device_list:
+        val_hist[device] = History(name=('test/' + str(device)))
 
+    # learning embedding checkpointer.
     ckpter = CheckPoint(model=model, optimizer=optimizer, path='{}/ckpt/batch_all_total_with_knn_exp'.format(ROOT_DIR),
                         prefix=ckpt_prefix, interval=1, save_num=1)
 
+    # training embedding network
     for epoch in range(1, embedding_epochs + 1):
         scheduler.step()
         train_loss, metrics = train_epoch(train_loader=train_batch_loader, model=model, loss_fn=loss_fn,
                                           optimizer=optimizer, log_interval=log_interval,
                                           metrics=[AverageNoneZeroTripletsMetric()])
-        train_logs = {}
-        train_logs['loss'] = train_loss
+        train_logs = {'loss': train_loss}
         for metric in metrics:
             train_logs[metric.name()] = metric.value()
         train_hist.add(logs=train_logs, epoch=epoch)
 
-        for device in ['a', 'b', 'c']:
+        for device in device_list:
             test_acc = kNN(model=model, train_loader=train_batch_loader, test_loader=test_loader[device], k=k)
             test_logs = {'acc': test_acc}
             val_hist[device].add(logs=test_logs, epoch=epoch)
@@ -117,11 +121,11 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
         train_hist.plot()
         logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, train_hist.name, str(train_hist.recent)))
 
-        for device in ['a', 'b', 'c']:
+        for device in device_list:
             val_hist[device].plot()
             logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, val_hist[device].name, str(val_hist[device].recent)))
 
-        ckpter.check_on(epoch=epoch, monitor='acc', loss_acc=val_hist['b'].recent)
+        ckpter.check_on(epoch=epoch, monitor='acc', loss_acc=val_hist['bc'].recent)
 
     # train classifier using learned embeddings.
     classify_model = networks.classifier()
@@ -131,7 +135,7 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
     classify_scheduler = lr_scheduler.StepLR(optimizer=classify_optimizer, step_size=30, gamma=0.5)
     classify_train_hist = History(name='classify_train/a')
     classify_val_hist = {}
-    for device in ['a', 'b', 'c']:
+    for device in device_list:
         classify_val_hist[device] = History(name=('classify_val/' + str(device)))
     classify_ckpter = CheckPoint(model=classify_model, optimizer=classify_optimizer,
                                  path='{}/ckpt/batch_all_total_with_knn_exp'.format(ROOT_DIR),
@@ -140,19 +144,21 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
     best_model_filename = Reporter(ckpt_root=os.path.join(ROOT_DIR, 'ckpt'), exp='batch_all_total_with_knn_exp').select_best(run=ckpt_prefix).selected_ckpt
     model.load_state_dict(torch.load(best_model_filename)['model_state_dict'])
 
+    # learned best embeddings
     train_embedding, train_labels = extract_embeddings(train_batch_loader, model, embed_dims)
     test_embedding, test_labels = {}, {}
-    for device in ['a', 'b', 'c']:
+    for device in device_list:
         test_embedding[device], test_labels[device] = extract_embeddings(test_loader[device], model, embed_dims)
 
+    # wrap embeddings(numpy) to Dataset
     classify_train_dataset = DatasetWrapper(data=train_embedding, labels=train_labels, transform=ToTensor())
     classify_test_dataset = {}
-    for device in ['a', 'b', 'c']:
+    for device in device_list:
         classify_test_dataset[device] = DatasetWrapper(data=test_embedding[device], labels=test_labels[device],
                                                        transform=ToTensor())
     classify_train_loader = DataLoader(dataset=classify_train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
     classify_test_loader = {}
-    for device in ['a', 'b', 'c']:
+    for device in device_list:
         classify_test_loader[device] = DataLoader(dataset=classify_test_dataset[device], batch_size=batch_size, shuffle=False,
                                                   num_workers=1)
 
@@ -165,7 +171,7 @@ def batch_all_total_with_knn_exp(device='0', ckpt_prefix='Run01', lr=1e-3, embed
 if __name__ == '__main__':
 
     kwargs = {
-        'ckpt_prefix': 'Run01',
+        'ckpt_prefix': 'Run02',
         'device': '0',
         'lr': 1e-3,
         'embedding_epochs': 1,
