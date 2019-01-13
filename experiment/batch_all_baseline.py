04cc129250d348cd314049a03d10ca7b4de4aece
hackerekcah <hackerekcah@outlook.com>
2019-01-14 07:31:53 +0800

    use xgb classifier, remove softmax classifier

--- experiment/batch_all_baseline.py
+++ experiment/batch_all_baseline.py
@@ -19,12 +19,13 @@ import logging
 from utils.history import *
 from utils.checkpoint import *
 from utils.utilities import *
+from experiment.xgb import xgb_cls
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def batch_all_with_knn_exp(device='3', ckpt_prefix='Run01', lr=1e-3, embedding_epochs=10, classify_epochs=100,
                            n_classes=10, n_samples=12, margin=0.3, log_interval=50, log_level="INFO", k=3,
-                           squared=False, embed_dims=64, embed_net='vgg'):
+                           squared=False, embed_dims=64, embed_net='vgg', is_train_embedding_model=False):
     """
     knn as classifier.
     :param device:
@@ -77,48 +78,38 @@ def batch_all_with_knn_exp(device='3', ckpt_prefix='Run01', lr=1e-3, embedding_e
         return
 
     model = model.cuda()
-    loss_fn = HardTripletLoss(margin=margin, hardest=False, squared=squared)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    scheduler = lr_scheduler.StepLR(optimizer=optimizer, step_size=30, gamma=0.5)
+    if is_train_embedding_model:
+        loss_fn = HardTripletLoss(margin=margin, hardest=False, squared=squared)
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+        scheduler = lr_scheduler.StepLR(optimizer=optimizer, step_size=30, gamma=0.5)
 
-    train_hist = History(name='train/a')
-    val_hist = History(name='test/a')
-    ckpter = CheckPoint(model=model, optimizer=optimizer, path='{}/ckpt/batch_all_with_knn_exp'.format(ROOT_DIR),
-                        prefix=ckpt_prefix, interval=1, save_num=1)
+        train_hist = History(name='train/a')
+        val_hist = History(name='test/a')
+        ckpter = CheckPoint(model=model, optimizer=optimizer, path='{}/ckpt/batch_all_with_knn_exp'.format(ROOT_DIR),
+                            prefix=ckpt_prefix, interval=1, save_num=1)
 
-    for epoch in range(1, embedding_epochs + 1):
-        scheduler.step()
-        train_loss, metrics = train_epoch(train_loader=train_batch_loader, model=model, loss_fn=loss_fn,
-                                          optimizer=optimizer, log_interval=log_interval,
-                                          metrics=[AverageNoneZeroTripletsMetric()])
-        train_logs = dict()
-        train_logs['loss'] = train_loss
-        for metric in metrics:
-            train_logs[metric.name()] = metric.value()
-        train_hist.add(logs=train_logs, epoch=epoch)
+        for epoch in range(1, embedding_epochs + 1):
+            scheduler.step()
+            train_loss, metrics = train_epoch(train_loader=train_batch_loader, model=model, loss_fn=loss_fn,
+                                              optimizer=optimizer, log_interval=log_interval,
+                                              metrics=[AverageNoneZeroTripletsMetric()])
+            train_logs = dict()
+            train_logs['loss'] = train_loss
+            for metric in metrics:
+                train_logs[metric.name()] = metric.value()
+            train_hist.add(logs=train_logs, epoch=epoch)
 
-        test_acc = kNN(model=model, train_loader=train_batch_loader, test_loader=test_batch_loader, k=k)
-        test_logs = {'acc': test_acc}
-        val_hist.add(logs=test_logs, epoch=epoch)
+            test_acc = kNN(model=model, train_loader=train_batch_loader, test_loader=test_batch_loader, k=k)
+            test_logs = {'acc': test_acc}
+            val_hist.add(logs=test_logs, epoch=epoch)
 
-        train_hist.clear()
-        train_hist.plot()
-        val_hist.plot()
-        logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, train_hist.name, str(train_hist.recent)))
-        logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, val_hist.name, str(val_hist.recent)))
-        ckpter.check_on(epoch=epoch, monitor='acc', loss_acc=val_hist.recent)
+            train_hist.clear()
+            train_hist.plot()
+            val_hist.plot()
+            logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, train_hist.name, str(train_hist.recent)))
+            logging.info('Epoch{:04d}, {:15}, {}'.format(epoch, val_hist.name, str(val_hist.recent)))
+            ckpter.check_on(epoch=epoch, monitor='acc', loss_acc=val_hist.recent)
 
-    # train classifier using learned embeddings.
-    classify_model = networks.classifier()
-    classify_model = classify_model.cuda()
-    classify_loss_fn = nn.CrossEntropyLoss()
-    classify_optimizer = optim.Adam(classify_model.parameters(), lr=lr)
-    classify_scheduler = lr_scheduler.StepLR(optimizer=classify_optimizer, step_size=30, gamma=0.5)
-    classify_train_hist = History(name='classify_train/a')
-    classify_val_hist = History(name='classify_val/a')
-    classify_ckpter = CheckPoint(model=classify_model, optimizer=classify_optimizer,
-                                 path='{}/ckpt/batch_all_with_knn_exp'.format(ROOT_DIR),
-                                 prefix=ckpt_prefix, interval=1, save_num=1)
     # reload best embedding model
     best_model_filename = Reporter(ckpt_root=os.path.join(ROOT_DIR, 'ckpt'), exp='batch_all_with_knn_exp').select_best(run=ckpt_prefix).selected_ckpt
     model.load_state_dict(torch.load(best_model_filename)['model_state_dict'])
@@ -126,22 +117,15 @@ def batch_all_with_knn_exp(device='3', ckpt_prefix='Run01', lr=1e-3, embedding_e
     train_embedding, train_labels = extract_embeddings(train_batch_loader, model, embed_dims)
     test_embedding, test_labels = extract_embeddings(test_batch_loader, model, embed_dims)
 
-    classify_train_dataset = DatasetWrapper(data=train_embedding, labels=train_labels, transform=ToTensor())
-    classify_test_dataset = DatasetWrapper(data=test_embedding, labels=test_labels, transform=ToTensor())
-    classify_train_loader = DataLoader(dataset=classify_train_dataset, batch_size=128, shuffle=True, num_workers=1)
-    classify_test_loader = DataLoader(dataset=classify_test_dataset, batch_size=128, shuffle=False, num_workers=1)
-
-    fit(train_loader=classify_train_loader, val_loader=classify_test_loader, model=classify_model,
-        loss_fn=classify_loss_fn, optimizer=classify_optimizer, scheduler=classify_scheduler, n_epochs=classify_epochs,
-        log_interval=log_interval, metrics=[AccumulatedAccuracyMetric()], train_hist=classify_train_hist,
-        val_hist=classify_val_hist, ckpter=classify_ckpter, logging=logging)
+    xgb_cls(train_data=train_embedding, train_label=train_labels, val_data=test_embedding, val_label=test_labels,
+            exp_dir=os.path.dirname(log_file))
 
 
 if __name__ == '__main__':
 
     kwargs = {
         'ckpt_prefix': 'Run02',
-        'device': '0',
+        'device': '5',
         'lr': 1e-3,
         'embedding_epochs': 1,
         'classify_epochs': 5,
